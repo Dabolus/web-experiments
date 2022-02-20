4194e72fe9a99816137c88209e279bd7b01a5fce
Giorgio Garasto <giorgio@garasto.me>
2022-02-20 10:09:10 +0100

    feat: setup home page package structure

---
+++ src/home/vite.config.ts
@@ -0,0 +1,11 @@
+// TODO: replace this path with @webexp/config/... once Vite
+// gets support for transpilation of same-monorepo packages
+// See: https://github.com/vitejs/vite/issues/5370
+import createConfig from '../config/vite/vanilla';
+import { fromYaml } from '../config/utils/yaml';
+
+export default createConfig(async () => ({
+  htmlData: {
+    projects: await fromYaml('src/data/projects.yml'),
+  },
+}));
