a24c606f2c4063a8436ff1a8688454b5c02ae03b
Giorgio Garasto <giorgio@garasto.me>
2022-02-28 21:43:57 +0100

    chore: setup dynamic firebase config generation in build script

--- scripts/build.js
+++ scripts/build.js
@@ -19,3 +19,30 @@ await Promise.all(
     fs.rename(`src/experiments/${project}/dist/`, `dist/${project}/`),
   ),
 );
+
+// Read the Firebase config template
+const firebaseConfigTemplate = JSON.parse(
+  await fs.readFile('firebase.template.json', 'utf8'),
+);
+
+// Dynamically add the rules for client-side routing for each project
+const firebaseConfig = {
+  ...firebaseConfigTemplate,
+  hosting: {
+    ...firebaseConfigTemplate.hosting,
+    rewrites: [
+      ...(firebaseConfigTemplate.hosting.rewrites || []),
+      ...projects.map(project => ({
+        source: `/${project}/**`,
+        destination: `/${project}/`,
+      })),
+    ],
+  },
+};
+
+// Write the final Firebase config
+await fs.writeFile(
+  'firebase.json',
+  JSON.stringify(firebaseConfig, null, 2),
+  'utf8',
+);
