d12cd9bfdda0177f38ed5dfcc91711701b0e5fe0
Giorgio Garasto <giorgio@garasto.me>
2022-10-12 15:38:24 -0300

    refactor: migrate home page to portfolio data package

--- src/home/vite.config.ts
+++ src/home/vite.config.ts
@@ -1,12 +1,15 @@
+import { projects } from '@dabolus/portfolio-data';
 // TODO: replace this path with @webexp/config/... once Vite
 // gets support for transpilation of same-monorepo packages
 // See: https://github.com/vitejs/vite/issues/5370
 import createConfig from '../config/vite/vanilla';
-import { fromYaml } from '../config/utils/yaml';
 
 export default createConfig(async () => ({
   base: '/',
   htmlData: {
-    projects: await fromYaml('src/data/projects.yml'),
+    projects: Object.entries(projects).map(([id, project]) => ({
+      id,
+      ...project,
+    })),
   },
 }));
