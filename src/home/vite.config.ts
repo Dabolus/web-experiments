2548354893673d6ffbc225ab420eae5b798b9a51
Giorgio Garasto <giorgio@garasto.me>
2022-10-14 19:57:35 +0200

    chore: update portfolio data package

--- src/home/vite.config.ts
+++ src/home/vite.config.ts
@@ -1,4 +1,8 @@
-import { projects } from '@dabolus/portfolio-data';
+import {
+  projects,
+  ProjectIconFormat,
+  ProjectType,
+} from '@dabolus/portfolio-data';
 // TODO: replace this path with @webexp/config/... once Vite
 // gets support for transpilation of same-monorepo packages
 // See: https://github.com/vitejs/vite/issues/5370
@@ -7,6 +11,8 @@ import createConfig from '../config/vite/vanilla';
 export default createConfig(async () => ({
   base: '/',
   htmlData: {
+    ProjectIconFormat,
+    ProjectType,
     projects: Object.entries(projects).map(([id, project]) => ({
       id,
       ...project,
